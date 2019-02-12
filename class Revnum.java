28abaf76aaafcac525140d5fede5de65f3a8335e
ljprabhu <43901449+ljprabhu@users.noreply.github.com>
2019-02-12 12:34:18 +0530

    Add files via upload

---
+++ class Revnum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Revnum
+{
+public static void main(String args[])
+{
+int num, rev=0, rem;
+Scanner scan = new Scanner(System.in);
+num = scan.nextInt();
+while(num != 0)
+{
+rem = num%10;
+rev = rev*10 + rem;
+num = num/10;
+}
+System.out.print(rev);
+}
+}
