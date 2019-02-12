f5d3331ba857e0d6c8ad5fc7696bb2f7e84861d6
ljprabhu <43901449+ljprabhu@users.noreply.github.com>
2019-02-12 12:38:01 +0530

    Add files via upload

---
+++ class String.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class String
+{
+public static void main(String args[])
+{
+String str;
+Scanner scan = new Scanner(System.in);
+str= scan.nextLine();
+System.out.print(str+".");
+}
+}
